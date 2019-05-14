ad27fb674adba9ca6d31aa6ad96ccbd65de94079
ViniciusRibSantos <ribeirosvinicius2015@gmail.com>
2019-05-14 13:55:38 -0300

    Mudança do front da index, foi adicionado editor html e correções ao carregar arquivos

--- src/main/java/br/usjt/ccp3an_mca_projeto_integrado/model/repository/IArquivoRepository.java
+++ src/main/java/br/usjt/ccp3an_mca_projeto_integrado/model/repository/IArquivoRepository.java
@@ -1,9 +1,15 @@
 package br.usjt.ccp3an_mca_projeto_integrado.model.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.usjt.ccp3an_mca_projeto_integrado.model.Arquivo;
 
 public interface IArquivoRepository extends JpaRepository<Arquivo, Long>{
 
+	@Query("SELECT a FROM Arquivo a JOIN a.extensao e JOIN e.tipoDeArquivo t WHERE t.id = :tipoDeArquivoID")
+	public List<Arquivo> carregarArquivosPorTipoDeArquivo(@Param ("tipoDeArquivoID") Long tipoDeArquivoID);
 }
